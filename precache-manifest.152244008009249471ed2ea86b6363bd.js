783a1b681f469ca1ec02fd2791c035ef63a0ffa1
walterperez <waltermaniaco@gmail.com>
2019-04-30 09:30:32 +0100

    Updates

---
+++ precache-manifest.152244008009249471ed2ea86b6363bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e49c986bca8b248990a54fef7258442d",
+    "url": "/XP-portfolio/index.html"
+  },
+  {
+    "revision": "ce35b3df68ccbc85105d",
+    "url": "/XP-portfolio/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/XP-portfolio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ce35b3df68ccbc85105d",
+    "url": "/XP-portfolio/static/js/main.e008feff.chunk.js"
+  },
+  {
+    "revision": "bcaf098646c27366da70",
+    "url": "/XP-portfolio/static/js/runtime~main.cd169f95.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/XP-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
